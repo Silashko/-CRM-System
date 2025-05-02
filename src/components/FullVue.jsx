24d0a387beac1de779e5c65b25efa0c2d0a44bb1
Kirill <silashkokirill00@mail.ru>
2025-05-02 21:12:46 +0300

    Added a splash screen

---
+++ src/components/FullVue.jsx
@@ -0,0 +1,14 @@
+import Input from "./Input.jsx";
+
+export default function FullVue() {
+  return (
+    <>
+      <Input />
+      <div>
+        <h2>Все (Х)</h2>
+        <h2>В работе (Х)</h2>
+        <h2>Выполнено (Х) </h2>
+      </div>
+    </>
+  );
+}
